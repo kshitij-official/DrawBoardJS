11596326a51f6637cbd36d1db3a674a2272b4e00
Kshitij Tripathi <kshitij.btech.cs19@iiitranchi.ac.in>
2021-10-15 17:19:14 +0530

    Final Draw

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 
+let rect = canvas.getBoundingClientRect();
 
-document.getElementById("canvas").addEventListener("mousedown", drawRectangle);
+document.getElementById("canvas").addEventListener("mousemove", draw);
+document.getElementById("canvas").addEventListener("mousedown", setPosition);
+document.getElementById("canvas").addEventListener("mouseenter", setPosition);
 
-function drawRectangle(e){
-    let rect = canvas.getBoundingClientRect();
-
-    var X = e.pageX - rect.left;
-    var Y = e.pageY - rect.top;
-    console.log(X, Y);
+document.querySelector(".Red").addEventListener('click', chooseColorRed);
+document.querySelector(".Green").addEventListener('click', chooseColorGreen);
+document.querySelector(".Blue").addEventListener('click', chooseColorBlue);
 
 
-    context.fillStyle ='rgba(0, 0, 54, 0.8)';
-    context.fillRect(X, Y, 5, 5);
+function chooseColorRed(){
+        context.strokeStyle='red';
+}
+function chooseColorGreen(){
+        context.strokeStyle='green';
+}
+function chooseColorBlue(){
+        context.strokeStyle='blue';
+}
+var pos = {x:0, y:0};
 
+function setPosition(e){
+    pos.x = e.pageX - rect.left;
+    pos.y = e.pageY - rect.top;
+}
+function draw(e){
+
+  
+
+    if(e.buttons !== 1) return;
+
+    context.beginPath();
+    context.lineWidth=5;
+    context.lineCap='round';
+    context.moveTo(pos.x, pos.y); //from
+    setPosition(e);
+    context.lineTo(pos.x, pos.y);
+    context.stroke();
 }
 
 
